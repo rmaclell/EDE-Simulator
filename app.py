0d78c0388440f728d8b9b9f0ba57c28425547196
rmaclell <rom00233@students.stir.ac.uk>
2023-08-02 17:04:15 +0100

    Pseudo-random request generation.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import random
+import json
 import sqlite3
-from flask import request, url_for, render_template
+import requests
+from flask import Flask, request, url_for, render_template
 from flask_api import FlaskAPI, status, exceptions
+from internal_services import generate_request_chain, construct_json_response, build_query_string
 
-app = FlaskAPI(__name__)
+app = Flask(__name__)
 
 # URL/URIs for the API endpoints.
 base_url = "http://127.0.0.1:5000"
@@ -11,6 +13,10 @@ low_sev_data = "/segments/low/"
 mod_sev_data = "/segments/mod/"
 high_sev_data = "/segments/mod"
 
+
+# Establish a connection to the local SQLite database.
+# connection = sqlite3.connect('data/ede_eval_data')
+# cur = connection.cursor()
 
 @app.route("/")
 def home():
@@ -20,7 +26,7 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/customers/<severity>/<num_attributes>")
+@app.route("/customers/<severity>/<int:num_attributes>")
 def customer_data(severity, num_attributes):
     """API endpoint that returns data depending on the exposure severity requested.
     :param severity: defines the severity of EDE, either none, low, mod or high.
@@ -28,20 +34,61 @@ def customer_data(severity, num_attributes):
     :return: an HTML template, JSON response and HTTP status code.
     """
     if num_attributes in range(1, 7):
-        if severity == "low":
-
-            return status.HTTP_200_OK
-        elif severity == "mod":
-            return status.HTTP_200_OK
-        elif severity == "high":
-            return status.HTTP_200_OK
-        elif severity == "none":
-            return status.HTTP_200_OK
-        else:
-            return status.HTTP_400_BAD_REQUEST
+        with sqlite3.connect('data/ede_eval_data') as connection:
+            cur = connection.cursor()
+            if severity == "low":
+                low_sev_attributes = []
+                data = cur.execute("SELECT * FROM low_severity")
+                for attribute in data.description:
+                    low_sev_attributes.append(attribute[0])
+                request_chain = generate_request_chain(severity, low_sev_attributes)
+                response_data = {}
+                for request_uri in request_chain:
+                    response = requests.get(request_uri)
+=
+                print(response_data)
+            elif severity == "mod":
+                return status.HTTP_200_OK
+            elif severity == "high":
+                return status.HTTP_200_OK
+            elif severity == "none":
+                return status.HTTP_200_OK
+            else:
+                return status.HTTP_400_BAD_REQUEST
     else:
         return status.HTTP_400_BAD_REQUEST
 
+
+customer_data("low", 5)
+
+
+@app.route("/segments/low/")
+def customer_segments_low():
+    attribute_list = request.args.getlist("attr")
+    with sqlite3.connect('data/ede_eval_data') as connection:
+        cur = connection.cursor()
+        cur.execute(build_query_string(attribute_list, "low"))
+        connection.commit()
+        response = construct_json_response(cur)
+        return response[0]
+
+
+@app.route("/segments/mod/<num_attributes>")
+def customer_segments_mod(num_attributes):
+    if num_attributes in range(1, 7):
+        cur.execute("SELECT * FROM mod_severity")
+        return status.HTTP_200_OK
+    else:
+        return status.HTTP_400_BAD_REQUEST
+
+
+@app.route("/segments/high/<num_attributes>")
+def customer_segments_high(num_attributes):
+    if num_attributes in range(1, 7):
+        cur.execute("SELECT * FROM high_severity")
+        return status.HTTP_200_OK
+    else:
+        return status.HTTP_400_BAD_REQUEST
 
 
 if __name__ == "__main__":
---
+++ internal_services.py
@@ -0,0 +1,53 @@
+import random
+
+# URL/URIs for the API endpoints.
+base_url = "http://127.0.0.1:5000"
+segments_uri = "/segments"
+
+
+def generate_request_chain(severity, attributes):
+    """Creates a pseudo-random request chain for internal services.
+    :param attributes:
+    :param severity: the severity of exposure selected.
+    :return: a string array containing the URIs for the request chain.
+    """
+    # Instantiate the maximum attributes and the request chain array.
+    max_attributes = 7
+    request_chain = []
+
+    while max_attributes > 0:
+        # Check the maximum attributes left is not one to prevent a value error in the random range function.
+        if max_attributes != 1:
+            no_req_attributes = random.randrange(1, max_attributes)
+            req_attributes = random.sample(attributes, no_req_attributes)
+            new_request = base_url + segments_uri + "/" + severity + "/?"
+            for attr_string in req_attributes:
+                new_request = new_request + "attr=" + attr_string + "&"
+                attributes.remove(attr_string)
+            new_request = new_request.rstrip(new_request[-1])
+            max_attributes = max_attributes - no_req_attributes
+            request_chain.append(new_request)
+        else:
+            new_request = base_url + segments_uri + "/" + severity + "/?attr=" + attributes[0]
+            request_chain.append(new_request)
+            break
+    return request_chain
+
+
+def construct_json_response(cursor):
+    response = [dict((cursor.description[i][0], value)
+                     for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return response
+
+
+def build_query_string(attribute_list, severity_lvl):
+    table_string = severity_lvl + "_severity"
+    column_string = attribute_list[0]
+    if len(attribute_list) > 1:
+        for attr_string in attribute_list:
+            column_string = column_string + "," + attr_string
+    return "SELECT " + column_string + " FROM " + table_string + " WHERE ROWID IN (SELECT ROWID from " + table_string + " ORDER BY random() LIMIT 1)"
+
+
+test_str = ["test1", "test2", "test2", "test3", "test4", "test5", "test6"]
+print(build_query_string(test_str, "low"))
