789d8f25357a60f25e39865b2376a3fb0a6bea83
rmaclell <rom00233@students.stir.ac.uk>
2023-08-03 16:34:13 +0100

    API implementation.

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-import json
+import random
 import sqlite3
-import requests
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, jsonify
 from flask_api import FlaskAPI, status, exceptions
-from internal_services import generate_request_chain, construct_json_response, build_query_string
+from internal_services import generate_request_chain, construct_json_response, handle_request_chain, get_attribute_list, \
+    fetch_data
 
 app = Flask(__name__)
 
-# URL/URIs for the API endpoints.
-base_url = "http://127.0.0.1:5000"
-low_sev_data = "/segments/low/"
-mod_sev_data = "/segments/mod/"
-high_sev_data = "/segments/mod"
+_MIN_ATTRIBUTES = 1
+_MAX_ATTRIBUTES = 7
+_SEVERITY_LVLS = ["low", "mod", "high"]
+
 
 @app.route("/")
 def home():
@@ -28,61 +27,31 @@ def customer_data(severity, num_attributes):
     :param num_attributes: the number of attributes requested.
     :return: an HTML template, JSON response and HTTP status code.
     """
-    if num_attributes in range(1, 7):
-        with sqlite3.connect('data/ede_eval_data') as connection:
-            cur = connection.cursor()
-            if severity == "low":
-                low_sev_attributes = []
-                data = cur.execute("SELECT * FROM low_severity")
-                for attribute in data.description:
-                    low_sev_attributes.append(attribute[0])
-                request_chain = generate_request_chain(severity, low_sev_attributes)
-                response_data = {}
-                for request_uri in request_chain:
-                    response = requests.get(request_uri)
-                print(response_data)
-            elif severity == "mod":
-                return status.HTTP_200_OK
-            elif severity == "high":
-                return status.HTTP_200_OK
-            elif severity == "none":
-                return status.HTTP_200_OK
-            else:
-                return status.HTTP_400_BAD_REQUEST
+    if num_attributes in range(_MIN_ATTRIBUTES, _MAX_ATTRIBUTES + 1):
+        if severity in _SEVERITY_LVLS:
+            attribute_list = get_attribute_list(severity)
+            request_chain = generate_request_chain(severity, attribute_list, len(attribute_list))
+            response_data = handle_request_chain(request_chain)
+            return jsonify(status.HTTP_200_OK, response_data)
+        elif severity == "none":
+            selected_entity = random.choice(_SEVERITY_LVLS)
+            attribute_list = get_attribute_list(selected_entity)
+            selected_attributes = random.sample(attribute_list, num_attributes)
+            request_chain = generate_request_chain(selected_entity, selected_attributes, len(selected_attributes))
+            response_data = handle_request_chain(request_chain)
+            return jsonify(status.HTTP_200_OK, response_data)
+        else:
+            return jsonify(status.HTTP_400_BAD_REQUEST, "The security level requested is not recognised."),
     else:
-        return status.HTTP_400_BAD_REQUEST
+        return jsonify(status.HTTP_400_BAD_REQUEST, "The requested URL is malformed.")
 
 
-customer_data("low", 5)
-
-
-@app.route("/segments/low/")
-def customer_segments_low():
+@app.route("/segments/<severity>/")
+def customer_segments(severity):
     attribute_list = request.args.getlist("attr")
-    with sqlite3.connect('data/ede_eval_data') as connection:
-        cur = connection.cursor()
-        cur.execute(build_query_string(attribute_list, "low"))
-        connection.commit()
-        response = construct_json_response(cur)
-        return response[0]
-
-
-@app.route("/segments/mod/<num_attributes>")
-def customer_segments_mod(num_attributes):
-    if num_attributes in range(1, 7):
-        cur.execute("SELECT * FROM mod_severity")
-        return status.HTTP_200_OK
-    else:
-        return status.HTTP_400_BAD_REQUEST
-
-
-@app.route("/segments/high/<num_attributes>")
-def customer_segments_high(num_attributes):
-    if num_attributes in range(1, 7):
-        cur.execute("SELECT * FROM high_severity")
-        return status.HTTP_200_OK
-    else:
-        return status.HTTP_400_BAD_REQUEST
+    data = fetch_data(severity, attribute_list)
+    response = construct_json_response(data)
+    return jsonify(response)
 
 
 if __name__ == "__main__":
--- internal_services.py
+++ internal_services.py
@@ -1,18 +1,40 @@
+import json
 import random
+import sqlite3
+
+import requests
 
 # URL/URIs for the API endpoints.
 base_url = "http://127.0.0.1:5000"
 segments_uri = "/segments"
 
 
-def generate_request_chain(severity, attributes):
+def get_attribute_list(severity):
+    attribute_list = []
+    with sqlite3.connect('data/ede_eval_data') as connection:
+        cur = connection.cursor()
+        data = cur.execute("SELECT * FROM " + severity + "_severity")
+        for attribute in data.description:
+            attribute_list.append(attribute[0])
+    return attribute_list
+
+
+def fetch_data(severity, attribute_list):
+    with sqlite3.connect('data/ede_eval_data') as connection:
+        cur = connection.cursor()
+        cur.execute(build_query_string(attribute_list, severity))
+    return cur
+
+
+def generate_request_chain(severity, attributes, no_attributes):
     """Creates a pseudo-random request chain for internal services.
+    :param no_attributes:
     :param attributes:
     :param severity: the severity of exposure selected.
     :return: a string array containing the URIs for the request chain.
     """
     # Instantiate the maximum attributes and the request chain array.
-    max_attributes = 7
+    max_attributes = no_attributes
     request_chain = []
     base_string = base_url + segments_uri + "/" + severity + "/?"
 
@@ -21,9 +43,9 @@ def generate_request_chain(severity, attributes):
         if max_attributes != 1:
             no_req_attributes = random.randrange(1, max_attributes)
             req_attributes = random.sample(attributes, no_req_attributes)
-            new_request = base_url + segments_uri + "/" + severity + "/?"
+            new_request = base_string
             for attr_string in req_attributes:
-                new_request = base_string + "attr=" + attr_string + "&"
+                new_request = new_request + "attr=" + attr_string + "&"
                 attributes.remove(attr_string)
             new_request = new_request.rstrip(new_request[-1])
             max_attributes = max_attributes - no_req_attributes
@@ -35,13 +57,26 @@ def generate_request_chain(severity, attributes):
     return request_chain
 
 
+def handle_request_chain(request_chain):
+    response_data = {}
+    for request_uri in request_chain:
+        response = requests.get(request_uri)
+        response_data.update(json.loads(response.json()))
+    return response_data
+
+
 def construct_json_response(cursor):
     response = [dict((cursor.description[i][0], value)
                      for i, value in enumerate(row)) for row in cursor.fetchall()]
-    return response
+    return json.dumps(response[0])
 
 
 def build_query_string(attribute_list, severity_lvl):
+    """Builds an SQL query string depending on the attributes passed and the requried severity level.
+    :param attribute_list: a list containing the attributes to be used in the column selection of the query
+    :param severity_lvl: a string value containing the severity level.
+    :return: a string containing the SQL query.
+    """
     table_string = severity_lvl + "_severity"
     column_string = attribute_list[0]
     if len(attribute_list) > 1:
